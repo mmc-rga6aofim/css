f740f09362f2aa18098ea9c487387dee03e70fc1
Alan Rodríguez <62192732+mmc-rga6aofim@users.noreply.github.com>
2023-05-23 20:59:03 -0600

    1
    1

---
+++ js/colorfondo.js
@@ -0,0 +1,71 @@
+var miElemento = document.getElementById('miElemento');
+var body = document.body;
+
+miElemento.addEventListener('mouseover', function() {
+  body.style.backgroundColor = 'blue';
+});
+
+miElemento.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+
+
+var miElemento2 = document.getElementById('miElemento2');
+var body = document.body;
+
+miElemento2.addEventListener('mouseover', function() {
+  body.style.backgroundColor ='blue';
+});
+
+miElemento2.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+
+
+var miElemento3 = document.getElementById('miElemento3');
+var body = document.body;
+
+miElemento3.addEventListener('mouseover', function() {
+  body.style.backgroundColor = 'blue';
+});
+
+miElemento3.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+4
+var miElemento4 = document.getElementById('miElemento4');
+var body = document.body;
+
+miElemento4.addEventListener('mouseover', function() {
+  body.style.backgroundColor = 'blue';
+});
+
+miElemento4.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+
+
+var miElemento3 = document.getElementById('miElemento3');
+var body = document.body;
+
+miElemento3.addEventListener('mouseover', function() {
+  body.style.backgroundColor = 'blue';
+});
+
+miElemento3.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+4
+var miElemento5 = document.getElementById('miElemento5');
+var body = document.body;
+
+miElemento5.addEventListener('mouseover', function() {
+  body.style.backgroundColor = 'blue';
+});
+
+miElemento5.addEventListener('mouseout', function() {
+  body.style.backgroundColor = '#f1f1f1';
+});
+
+
+
